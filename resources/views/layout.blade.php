576d8fff10eea743f7c00d438eea4939b133122e
Said Hammane <said.hammane1@gmail.com>
2023-08-30 18:44:42 +0100

    seo things

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @include('partials.header')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name=" robots" content=" index, follow">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Votre carrière est notre priorité" />
+    <meta property="og:description" content="Le site de recrutement des centres d'appels au Maroc" />
+    <meta property="og:image" content="https://marocareer.000webhostapp.com/img/logo_32_32.png" />
+    <meta property="og:url" content="https://marocareer.000webhostapp.com/" />
+    <meta property="og:site_name" content="marocareer" />
+    <meta name="keywords"
+        content="recherche d'emploi, opportunités d'emploi, offres de carrière, annonces d'emploi, postes vacants, chercheur d'emploi, 
+        portail d'emploi, base de données d'emploi, annonces de travail, possibilités d'emploi, candidatures, moteur de recherche d'emploi, 
+        site d'offres d'emploi, marché du travail, chasse à l'emploi, demandeurs d'emploi, site d'annonces d'emploi, trouver un emploi, 
+        plateforme de recherche d'emploi, correspondance d'emploi, site de publication d'emploi">
 
-        <title>MaroCareer</title>
 
-    </head>
-    <body>
-        @yield('content')
-        @include('partials.script')
-    </body>
+    <link rel="canonical" href="https://marocareer.000webhostapp.com/" />
+
+    @include('partials.header')
+
+    <title>MaroCareer</title>
+
+</head>
+
+<body>
+    @yield('content')
+    @include('partials.script')
+</body>
+
 </html>
