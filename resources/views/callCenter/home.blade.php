bab2a01369a97752cb00c94c38ae9630ee2d357c
oussammma <oussammma5@gmil.com>
2023-12-04 02:12:02 +0100

    add ads code

--- resources/views/callCenter/home.blade.php
+++ resources/views/callCenter/home.blade.php
@@ -7,6 +7,12 @@
 <!-- Start post Area -->
 <section class="post-area section-gap">
     <div class="container">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9270875061752923" crossorigin="anonymous"></script>
+        <!-- new one -->
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9270875061752923" data-ad-slot="1016599008" data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <div class="row justify-content-center d-flex my-5">
             <div class="col-lg-8 post-list">
                 <div class="menu-content pb-30 col-lg-12">
