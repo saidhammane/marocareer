35eae44db3453cbf809c74a8bdaf4f87528d551f
oussammma <oussammma5@gmil.com>
2023-12-05 17:33:03 +0100

    update live site

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
     <meta name=" robots" content=" index, follow">
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Votre carrière est notre priorité" />
+    <meta property="description" content="Le site de recrutement des centres d'appels au Maroc" />
     <meta property="og:description" content="Le site de recrutement des centres d'appels au Maroc" />
     <meta property="og:image" content="https://marocareer.tech/img/logo_32_32.png" />
     <meta property="og:url" content="https://marocareer.tech" />
@@ -15,10 +16,9 @@
         portail d'emploi, base de données d'emploi, annonces de travail, possibilités d'emploi, candidatures, moteur de recherche d'emploi, 
         site d'offres d'emploi, marché du travail, chasse à l'emploi, demandeurs d'emploi, site d'annonces d'emploi, trouver un emploi, 
         plateforme de recherche d'emploi, correspondance d'emploi, site de publication d'emploi">
-    <!-- <meta name="google-site-verification" content="QVLLhWTmyC-ICoj3EKYwb6vcM5e2NbwuUNm65V8O9-g">
+        <link rel="canonical" href="https://marocareer.tech/" />
+    <meta name="google-site-verification" content="QVLLhWTmyC-ICoj3EKYwb6vcM5e2NbwuUNm65V8O9-g">
     <meta name="google-adsense-account" content="ca-pub-9270875061752923">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9270875061752923" crossorigin="anonymous"></script> -->
-    <link rel="canonical" href="https://marocareer.tech/" />
     @include('partials.header')
     <title>MaroCareer</title>
 </head>
